4ad282e6fa62d82a52ff8859950cfe8e8f707199
Oscar Escorcia Palencia <oe319840@gmail.com>
2025-10-30 18:12:09 -0500

    I just center the text in the page and other ones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <h3 className="text-7xl font-bold mb-8 text-yellow-300 [text-shadow:3px_3px_0_#000,-1px_-1px_0_#000,1px_-1px_0_#000,-1px_1px_0_#000,1px_1px_0_#000]">
           Simpson Quotes
         </h3>
-        <h3 className="bold text-2xl">
+        <h3 className="bold text-2xl text-center">
           The Simpsons universe in one project, Quotes, Characters, Episodes,{" "}
           <br /> Locations and Iconic phrases from Springfield
         </h3>
